44f5b4f5eed72fb74d72919662674f56a7a4be51
Bursur <30567351+Bursur@users.noreply.github.com>
2024-02-02 13:45:39 +0000

    Added the About button and cleaned up resources

--- webApp/src/jsWasmMain/kotlin/Common.kt
+++ webApp/src/jsWasmMain/kotlin/Common.kt
@@ -1,10 +1,13 @@
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import spiragps.data.destinations.SpiraDestinations
 import spiragps.data.route.NavigationState
 import spiragps.data.route.rememberNavigationState
@@ -35,8 +38,10 @@ internal fun SpiraGPS() {
                     NavigationState.EDITOR -> EditorPage(navigationState)
                 }
 
-                //HelpActionButton(modifier = Modifier.align(Alignment.BottomEnd))
-                SettingsActionButton(modifier = Modifier.align(Alignment.BottomEnd))
+                Row(modifier = Modifier.align(Alignment.BottomEnd).padding(10.dp)) {
+                    HelpActionButton()
+                    SettingsActionButton()
+                }
             }
         }
         else
--- webApp/src/jsWasmMain/kotlin/spiragps/dialogs/SettingsDialog.kt
+++ webApp/src/jsWasmMain/kotlin/spiragps/dialogs/SettingsDialog.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -63,7 +65,7 @@ fun SettingsActionButton(modifier: Modifier = Modifier) {
         contentColor = SpiraGPSColours.value.fabIconColour,
         modifier = modifier
     ) {
-        Image(painter = painterResource("SpiraGPS/settings.png"), contentDescription = null, modifier = Modifier.padding(10.dp))
+        Image(painter = painterResource("SpiraGPS/settings.png"), contentDescription = null, modifier = Modifier.padding(5.dp).width(24.dp).height(24.dp))
     }
 
     if(openAlertDialog)
