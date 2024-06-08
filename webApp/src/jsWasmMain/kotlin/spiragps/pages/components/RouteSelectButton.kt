745db44511af6eec4da9dc2ebb4c65e14ffe5167
Bursur <30567351+Bursur@users.noreply.github.com>
2024-06-08 19:07:46 +0100

    Added a copy link system

--- webApp/src/jsWasmMain/kotlin/spiragps/pages/components/RouteSelectButton.kt
+++ webApp/src/jsWasmMain/kotlin/spiragps/pages/components/RouteSelectButton.kt
@@ -1,5 +1,6 @@
 package spiragps.pages.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
@@ -11,15 +12,24 @@ import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RichTooltipBox
+import androidx.compose.material3.RichTooltipState
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -27,18 +37,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import spiragps.data.destinations.Destination
 import spiragps.data.route.NavigationState
 import spiragps.style.SpiraGPSColours
 import spiragps.style.SpiraGPSText
+import spiragps.style.SpiraGPSVersion
 
-@OptIn(ExperimentalResourceApi::class)
+external fun setClipboard(url: String)
+
+@OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun RouteSelectButton(destination: Destination, navigationState: NavigationState) {
     val bgColour = animateColorAsState(SpiraGPSColours.value.infoBackground)
     val textColour = animateColorAsState(SpiraGPSColours.value.text)
+    val tooltipState = remember { RichTooltipState() }
+    val scope = rememberCoroutineScope()
     val interactionSource = remember { MutableInteractionSource() }
     val isHovered by interactionSource.collectIsHoveredAsState()
 
@@ -73,7 +89,36 @@ fun RouteSelectButton(destination: Destination, navigationState: NavigationState
                 modifier = Modifier.align(Alignment.BottomCenter),
                 isExpanded = isHovered
             )
+
+            AnimatedVisibility(visible = isHovered, modifier = Modifier.align(Alignment.TopEnd)) {
+                RichTooltipBox(
+                    text = { Text("Copied to clipboard!", style = SpiraGPSText.typography.value.info, color = textColour.value) },
+                    tooltipState = tooltipState,
+                    colors = TooltipDefaults.richTooltipColors(containerColor = bgColour.value),
+                ) {
+                    Image(
+                        painter = painterResource("SpiraGPS/link.png"),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .width(24.dp)
+                            .height(24.dp)
+                            .background(SpiraGPSColours.value.fabBackgroundColour, CircleShape)
+                            .clickable {
+                                setClipboard("https://bursur.github.io/SpiraGPS/?route_id=${destination.id}")
+                                scope.launch {
+                                    tooltipState.show()
+                                }
+                            }
+                    )
+                }
+            }
         }
+    }
+
+    LaunchedEffect(isHovered) {
+        if(!isHovered)
+            tooltipState.dismiss()
     }
 }
 
