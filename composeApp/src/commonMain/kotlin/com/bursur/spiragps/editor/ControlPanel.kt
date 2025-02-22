859f776d1ae5b10f6368a8e6f7ceab18d3bc0eac
Bursur <30567351+Bursur@users.noreply.github.com>
2025-02-22 15:42:54 +0000

    added support for the start minimised toggle

--- composeApp/src/commonMain/kotlin/com/bursur/spiragps/editor/ControlPanel.kt
+++ composeApp/src/commonMain/kotlin/com/bursur/spiragps/editor/ControlPanel.kt
@@ -4,16 +4,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -21,6 +22,7 @@ import com.bursur.spiragps.route.conditions.ConditionSelector
 import com.bursur.spiragps.route.data.Condition
 import com.bursur.spiragps.route.data.Entry
 import com.bursur.spiragps.theme.SpiraGPSColours
+import com.bursur.spiragps.theme.SpiraGPSText
 
 @Composable
 fun ControlPanel(
@@ -31,8 +33,9 @@ fun ControlPanel(
     onMoveDown: (Entry) -> Unit
 ) {
     var conditionExpanded by remember { mutableStateOf(false) }
+    var selected by remember { mutableStateOf(entry.minimised) }
 
-    Row {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         // Conditions
         ControlPanelButton("https://bursur.github.io/SpiraGPS/todo.png") {
             conditionExpanded = true
@@ -40,10 +43,10 @@ fun ControlPanel(
 
         // Start Minimised (Panel only)
         if(isPanel(entry)) {
-            Icon(
-                imageVector = Icons.Default.List,
-                contentDescription = null
-            )
+            ControlPanelToggle(selected) {
+                selected = it
+                entry.minimised = it
+            }
         }
 
         // Up
@@ -79,5 +82,24 @@ private fun ControlPanelButton(icon: String, onClick: () -> Unit) {
         )
     ) {
         AsyncImage(model = icon, contentDescription = null, modifier = Modifier.padding(5.dp).width(24.dp).height(24.dp))
+    }
+}
+
+@Composable
+private fun ControlPanelToggle(selected: Boolean, onClick: (Boolean) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Start Minimised:", style = SpiraGPSText.typography.info, color = SpiraGPSColours.text)
+        Checkbox(
+            checked = selected,
+            onCheckedChange = {
+                onClick(it)
+            },
+            colors = CheckboxDefaults.colors(
+                uncheckedColor = SpiraGPSColours.toggleUnselectedTrackColour,
+                checkedColor = SpiraGPSColours.toggleSelectedTrackColour
+            )
+        )
     }
 }
