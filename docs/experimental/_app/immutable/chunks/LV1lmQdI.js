764f3b3cf9653d2b55a0730b02da3223b8832b1d
Bursur <30567351+Bursur@users.noreply.github.com>
2025-09-06 12:07:12 +0100

    experimental attempt again

---
+++ docs/experimental/_app/immutable/chunks/LV1lmQdI.js
@@ -0,0 +1,1 @@
+import{v as a}from"./CMAo_ZkR.js";a();
---
+++ docs/experimental/_app/immutable/nodes/4.swpNSPBC.js
@@ -0,0 +1,1 @@
+import"../chunks/BSvYFtAb.js";import"../chunks/LV1lmQdI.js";import{E as r}from"../chunks/DChk2YqS.js";function m(o){r(o)}export{m as component};
